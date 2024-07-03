ab76b5d92a42c786c3316d9e9f0d43098a075840
TejaswiPannala <pannalatejaswireddy@gmail.com>
2024-07-03 19:15:45 +0530

    Commit Message

---
+++ CoreJava/src/com/tnsif/encapsulation/Demo.java
@@ -0,0 +1,12 @@
+package com.tnsif.encapsulation;
+
+public class Demo {
+	public static void main(String[] args) {
+		Encapsulationdemo e=new Encapsulationdemo();
+		e.serialnumber=1;
+		e.name="teju";
+		e.age=20;
+		e.show();
+	}
+
+}
