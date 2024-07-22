f780158a08fb4c21c1ee30c60fbb42391e2a872e
TejaswiPannala <pannalatejaswireddy@gmail.com>
2024-07-22 15:30:59 +0530

    commit message

---
+++ CoreJava/src/com/tnsif/setdemo/Treesetdemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.setdemo;
+import java.util.TreeSet;
+public class Treesetdemo {
+	public static void treeoperation() {
+		TreeSet<Integer> h=new TreeSet<Integer>();
+		h.add(2);
+		h.add(8);
+		h.add(1);
+		h.add(5);
+		//h.add(null);
+		System.out.println("treeset element "+h);
+	}
+
+}
